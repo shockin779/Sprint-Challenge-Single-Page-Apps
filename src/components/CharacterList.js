405c3f7c79f9bee7029024379ca7e390c0771e63
Sean Hockin <sean.hockin@gmail.com>
2019-10-11 22:07:41 -0600

    MVP Complete. Search implemented

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import { setState } from "expect/build/jestMatchersObject";
+import SearchForm from './SearchForm';
 import CharacterCard from './CharacterCard';
 
 import './CharacterList.css';
@@ -8,6 +9,7 @@ import './CharacterList.css';
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState(null);
+  const [searchedCharacter, setSearchedCharacter] = useState(characters);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
@@ -24,13 +26,46 @@ export default function CharacterList() {
     return <p>Loading characters. Please wait...</p>
   }
 
-  return (
-    <section className="character-list">
-      {
-        characters.map(character => (
-          <CharacterCard key={character.id} character={character} />
-        ))
+  const handleChange = (event) => {
+    event.preventDefault();
+    let searchedCharacter = event.target.value.toLowerCase();
+    setSearchedCharacter(searchedCharacter);
+  }
+
+  if(searchedCharacter) {
+    console.log(characters)
+    let charactersFiltered = characters.filter(character => {
+      if(character.name.toLowerCase().includes(searchedCharacter)){
+        return character;
       }
-    </section>
+  }
+  )
+  console.log(charactersFiltered);
+
+    return (
+      <div className='characterListPage'>
+      <SearchForm handleChange={handleChange} />
+      <section className="character-list">
+        {
+          charactersFiltered.map(character => (
+            <CharacterCard key={character.id} character={character} />
+          ))
+        }
+      </section>
+      </div>
+    )
+  }
+
+  return (
+    <div className='characterListPage'>
+      <SearchForm handleChange={handleChange} />
+      <section className="character-list">
+        {
+          characters.map(character => (
+            <CharacterCard key={character.id} character={character} />
+          ))
+        }
+      </section>
+    </div>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import Form from 'react-bootstrap/Form';
 
-export default function SearchForm() {
+export default function SearchForm(props) {
  
   return (
     <section className="search-form">
-     // Add a search form here
+      <Form>
+        <Form.Control type='text' placeholder='Search for character' onChange={props.handleChange} />
+      </Form>
     </section>
   );
 }
