4f21ffe1fe2f4e461b8b55ae01dfad84f95b4d26
Sean Hockin <sean.hockin@gmail.com>
2019-10-11 21:35:16 -0600

    Implemented React Strap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from './components/CharacterList';
+import WelcomePage from './components/WelcomePage';
+import { Route } from 'react-router-dom';
+
+//Import bootstrap for ReactStrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <Route exact path='/' component={WelcomePage} />
+      <Route path='/characters' component={CharacterList} />
     </main>
   );
 }
